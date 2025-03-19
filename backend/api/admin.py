47edb7ccf19dd9b7d68eecfe676fe4afd6f3d98d
topdev00000 <adrian.gortopan@gmail.com>
2025-03-20 08:27:45 +0900

    feature/init

--- backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Message
-
-@admin.register(Message)
-class MessageModelAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'subject', 'created_at', 'updated_at')
-    list_filter = ('created_at',)
-    search_fields = ('subject', 'body')
